37548945de1b10ddb9896a6c378603c4804e57be
Svetlana <fidelity-67rus@mail.ru>
2022-08-07 14:37:59 +0400

    feat: add clock and calendar

---
+++ momentum/js/script.js
@@ -0,0 +1,20 @@
+const time = document.querySelector('.time');
+const day = document.querySelector('.date');
+
+function showTime() {
+  const date = new Date();
+  const currentTime = date.toLocaleTimeString();
+  time.textContent = currentTime;
+  setTimeout(showTime, 1000);
+  showDate();
+}
+showTime();
+
+function showDate() {
+  const date = new Date();
+  const options = {weekday: 'long', month: 'long', day: 'numeric', timeZone: 'UTC'};
+  const currentDate = date.toLocaleDateString('en-US', options);
+  day.textContent = currentDate;
+  setTimeout(showDate, 1000);
+}
+
